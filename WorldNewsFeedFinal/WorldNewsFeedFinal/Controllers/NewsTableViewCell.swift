069f92823ac3ede5b57e2e1617e30b3ff1812c72
KristaB77 <87663631+KristaB77@users.noreply.github.com>
2021-08-25 18:55:13 +0300

    Add files via upload

---
+++ WorldNewsFeedFinal/WorldNewsFeedFinal/Controllers/NewsTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewsTableViewCell.swift
+//  WorldNewsFeedFinal
+//
+//  Created by krista.bolsakova on 22/08/2021.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+    @IBOutlet weak var newsTitleLabel: UILabel!
+    @IBOutlet weak var newsImageView: UIImageView!
+    
+}
